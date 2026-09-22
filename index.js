09083ea0ba057e027c26c3e72dfda19b65e93337
Kylia Miskell <8714037+kmiskell@users.noreply.github.com>
2026-09-22 16:08:52 -0700

    Start integrating DB querying into the backend

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 app.use(express.json()) // This adds the json-parser to the app
 app.use(express.static('dist'))
+
+const password = process.argv[2]
+const url = `mongodb+srv://kyliamiskell_db_user:${password}@cluster0.yhznngn.mongodb.net/noteApp?appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, {family: 4})
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 let notes = [
   {
@@ -27,7 +41,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes=> {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
