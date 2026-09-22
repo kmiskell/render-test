fcf5354941401a6db42cdcba132a459cb9b01594
Kylia Miskell <8714037+kmiskell@users.noreply.github.com>
2026-09-22 16:21:51 -0700

    Installed dotenv

--- index.js
+++ index.js
@@ -1,48 +1,9 @@
 const express = require('express')
 const app = express()
-const mongoose = require('mongoose')
+const Note = require('./models/note')
 
 app.use(express.json()) // This adds the json-parser to the app
 app.use(express.static('dist'))
-
-const password = process.argv[2]
-const url = `mongodb+srv://kyliamiskell_db_user:${password}@cluster0.yhznngn.mongodb.net/noteApp?appName=Cluster0`
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url, {family: 4})
-
-const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-})
-
-noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
-})
-
-const Note = mongoose.model('Note', noteSchema)
-
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy - this is coming from the server",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
