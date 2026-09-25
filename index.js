8880be48f3fa991b04e818a82197e9ec8e8fe233
Kylia Miskell <8714037+kmiskell@users.noreply.github.com>
2026-09-25 10:35:25 -0700

    Move error handling to Express middleware

--- index.js
+++ index.js
@@ -17,10 +17,16 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    Note.findById(request.params.id).then(note => {
-        response.json(note)
-    })
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -54,6 +60,17 @@ app.post('/api/notes', (request, response) => {
         response.json(savedNote)
     })
 })
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'})
+    }
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
